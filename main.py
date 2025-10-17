8ee442e76d6eb302efea6b0c7ebdb22a598fca58
weirdrazak <razakappiah2002@gmail.com>
2025-10-17 12:47:12 +0000

    Added FastAPI

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+import pandas as pd
+
+
+model = joblib.load("model.pk1")
+
+
+app = FastAPI()
+
+class IrisInput(BaseModel):
+    SepalLengthCm: float
+    SepalWidthCm: float
+    PetalLengthCm: float
+    PetalWidthCm: float
+
+species_mapping = {0: "Iris Setosa", 1: "Iris Versicolor", 2: "Iris Virginica"}
+
+@app.get("/")
+async def home():
+    """
+    Home endpoint to check if the server is running.
+    """  
+    return {"message": "Welcome to the Iris Species Predictor API"}
+
+@app.post("/predict")
+async def predict_species(input_data: IrisInput):
+    """
+    Predict endpoint to classify the Iris species.
+    Accepts JSON input and returns the predicted species.
+    """
+    # # Converts input data to numpy array
+    # input_array = np.array([[input_data.sepal_length,
+    #                          input_data.sepal_width,
+    #                          input_data.petal_length,
+    #                          input_data.petal_width]])                              
+    
+    feature_names = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
+    input_df = pd.DataFrame([input_data.dict()], columns=feature_names)
+
+    #Predict using the model
+    prediction = model.predict(input_df)[0] 
+
+    #Get the species name
+    species = species_mapping.get(prediction, "Unknown")
+
+    return {"prediction": species}
